11b4cd3ed5a5c8b8884e2bb0cca26d49287f3b1e
Fayaz Sardar <fyz.srd@gmail.com>
2025-08-17 15:17:34 +0530

    feat:User Otp Auth

--- src/controller/user/auth.controller.js
+++ src/controller/user/auth.controller.js
@@ -1,18 +1,76 @@
 import * as authService from '../../services/user/auth.service.js'
+import { isValidPhoneNumber, parsePhoneNumberFromString } from 'libphonenumber-js';
+
+//userAuth
+
+export const generateOtp = async (req, res) => {
+    try {
+        const { mobileNumber, countryCode } = req.body;
+
+        if (!mobileNumber || !countryCode) return res.status(400).json({ success: false, message: "Mobile number is required" });
 
 
-export const generateOtp = async (req,res)=>{
-    try{
+        const validNumber = isValidPhoneNumber(mobileNumber, countryCode)
 
-        const otp=12354
+        if (!validNumber) return res.status(400).json({ success: false, message: "Enter Valid Mobile Number" });
 
-        res.status(200).json({succes:true,
-            Otp:otp
+
+        const phoneObj = parsePhoneNumberFromString(mobileNumber, countryCode)
+        if (!phoneObj) {
+            return res.status(400).json({ success: false, message: "Invalid number" });
+        }
+
+        const e164Number = phoneObj.number;
+
+
+        const otp = await authService.sendOtp(e164Number)
+
+        res.status(200).json({ success: true, message: "OTP sent successfully", otp });
+
+    } catch (error) {
+        res.status(500).json({
+            succes: false,
+            message: error.message
         })
+    }
+}
 
-    }catch(error){
-        res.status(500).json({succes:false ,
-            message:error.message
+export const verifyotp = async (req, res) => {
+    try {
+        const { mobileNumber, countryCode, otp } = req.body;
+
+        if (!mobileNumber || !countryCode || !otp) {
+            return res.status(400).json({ success: false, message: "All fields are required" });
+        }
+
+        
+        const validNumber = isValidPhoneNumber(mobileNumber, countryCode);
+        if (!validNumber) {
+            return res.status(400).json({ success: false, message: "Enter valid mobile number" });
+        }
+
+        
+        const phoneObj = parsePhoneNumberFromString(mobileNumber, countryCode);
+        if (!phoneObj) {
+            return res.status(400).json({ success: false, message: "Invalid phone number" });
+        }
+        const e164Number = phoneObj.number;
+
+       
+        const result = await authService.verifyOtp(e164Number, otp)
+
+        return res.status(200).json({
+            success: true,
+            message: "OTP verified successfully",
+            user: result.user,
+            token: result.token, 
+        });
+
+
+    } catch (error) {
+        res.status(500).json({
+            succes: false,
+            message: error.message
         })
     }
 }
--- src/routes/user/auth.routes.js
+++ src/routes/user/auth.routes.js
@@ -1,8 +1,9 @@
 import express from 'express'
-import {generateOtp } from '../../controller/user/auth.controller.js'
+import {generateOtp,verifyotp } from '../../controller/user/auth.controller.js'
 const router=express.Router();
 
 
 router.post('/',generateOtp)
+router.post('/verify',verifyotp)
 
 export default router
