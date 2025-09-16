c232a5dcc53dd6484a6f8185e91ceb0706c87457
Fayaz Sardar <fyz.srd@gmail.com>
2025-09-16 12:59:24 +0530

    Ref: Category Api

--- src/routes/admin/category.routes.js
+++ src/routes/admin/category.routes.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import {addCategory,getCategoriesController,getCategory,updateCategory,deleteCategory} from '../../controller/admin/category.controller.js'
 import {protect} from '../../middlewares/authMiddleware.js'
+import upload from '../../middlewares/multer.js';
 const router = express.Router();
 
-router.post('/', addCategory);  // POST /api/admin/category
+router.post('/',upload.single("thumbnail"), addCategory);  // POST /api/admin/category
 router.get('/',getCategoriesController)
 router.get('/:id',getCategory ); 
-router.put('/:id',updateCategory)
+router.put('/:id',upload.single("thumbnail"),updateCategory)
 router.delete('/:id',deleteCategory)
 export default router;
